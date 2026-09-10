99c9e97f4ce178926f878c704c3979c58072e3d2
saloni-chaudhari09 <root@ip-172-31-31-12.ap-south-1.compute.internal>
2026-09-10 06:28:55 +0000

    Update application message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,6 +4,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello from Java Gradle Application!");
-        System.out.println("Build and deployment automated using Gradle and Jenkins.");
+        System.out.println("Build and deployment automated using Gradle and Jenkins - version 2.");
     }
 }
