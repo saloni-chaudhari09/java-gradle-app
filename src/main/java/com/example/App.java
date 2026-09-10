e01d062a9f5eb32832ed68e5693866fbf21cfd13
saloni-chaudhari09 <root@ip-172-31-31-12.ap-south-1.compute.internal>
2026-09-10 05:56:07 +0000

    Initial Java Gradle application

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Gradle Application!");
+        System.out.println("Build and deployment automated using Gradle and Jenkins.");
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testApplication() {
+        String message = "Hello from Java Gradle Application!";
+        assertEquals("Hello from Java Gradle Application!", message);
+    }
+}
